83f4219ca5527d5bd6f5882b5f05c3ac3bce2a69
Wolfgang Becker <wolfgang@cobalt.io>
2019-10-20 20:44:51 +0200

    Add feed endpoints and configure aws

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -1,16 +1,14 @@
 import AWS = require('aws-sdk');
 import { config } from './config/config';
 
-const c = config.dev;
-
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+var credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
-  region: c.aws_region,
-  params: {Bucket: c.aws_media_bucket}
+  region: config.aws_region,
+  params: {Bucket: config.aws_media_bucket}
 });
 
 
@@ -25,7 +23,7 @@ export function getGetSignedUrl( key: string ): string{
   const signedUrlExpireSeconds = 60 * 5
 
     const url = s3.getSignedUrl('getObject', {
-        Bucket: c.aws_media_bucket,
+        Bucket: config.aws_media_bucket,
         Key: key,
         Expires: signedUrlExpireSeconds
       });
@@ -44,7 +42,7 @@ export function getPutSignedUrl( key: string ){
     const signedUrlExpireSeconds = 60 * 5
 
     const url = s3.getSignedUrl('putObject', {
-      Bucket: c.aws_media_bucket,
+      Bucket: config.aws_media_bucket,
       Key: key,
       Expires: signedUrlExpireSeconds
     });
