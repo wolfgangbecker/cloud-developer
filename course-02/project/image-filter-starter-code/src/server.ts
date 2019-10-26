d571e9bc7d50b59c8766e6e50bacdab5ab43f7a9
Wolfgang Becker <wolfgang@cobalt.io>
2019-10-26 19:47:55 +0200

    Implement GET /filteredimage endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -28,15 +28,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage", async (req, res) => {
+    const {image_url} = req.query;
 
+    if(!image_url)
+      return res.status(422).send({error: "Missing image URL"});
+
+    try {
+      const file = await filterImageFromURL(image_url);
+      res.sendFile(file, () => {
+        deleteLocalFiles([file]);
+      });
+    } catch {
+      res.status(500).send({error: "Unable to filter image"});
+    }
+  });
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
